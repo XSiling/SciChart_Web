a498f9b349512d6001184490376ed61696015816
XSiling <570025307@qq.com>
2023-02-15 20:32:48 +0800

    set the password

--- HelloWorld/HelloWorld/settings.py
+++ HelloWorld/HelloWorld/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'runoob',
         'HOST': '127.0.0.1',
-        'PORT': 3306,
+        'PORT': 3340,
         'USER': 'root',
         'PASSWORD': '196910',
     }
